4130e1f9da94f60e0db60f01a7c975f276ae4985
unknown <viliusrbu@gmail.com>
2022-03-28 16:52:10 +0300

    remade to OOP

---
+++ second/classes/class.php
@@ -0,0 +1,45 @@
+<?php
+
+class ListClass
+{
+    public $list;
+    public $sorted_data;
+    public $arrayLists;
+
+    function __construct($list)
+    {
+        $this->list = $list;
+        $this->sorted_data = $list;
+        rsort($this->sorted_data);
+    }
+
+    public function create_lists($group_count)
+    {
+        $arrayLists = [];
+        for ($list = 0; $list < $group_count; $list++) {
+            array_push($arrayLists, array());
+        }
+        return $this->arrayLists = $arrayLists;
+    }
+
+    public function smallest_array()
+    {
+        $lowestSum = array_sum($this->arrayLists[0]);
+        $lowestArrayKey = 0;
+        for ($i = 1; $i < count($this->arrayLists); $i++) {
+            $sum = array_sum($this->arrayLists[$i]);
+            if ($sum  < $lowestSum) {
+                $lowestSum = $sum;
+                $lowestArrayKey = $i;
+            }
+        }
+        return $lowestArrayKey;
+    }
+    public function group_items()
+    {
+        for ($i = 0; $i < count($this->sorted_data); $i++) {
+            array_push($this->arrayLists[$this->smallest_array()], $this->sorted_data[$i]);
+        }
+        return $this->arrayLists;
+    }
+}
--- second/main.php
+++ second/main.php
@@ -1,47 +1,9 @@
 <?php
-
+include 'classes/class.php';
 $data = [1, 2, 4, 7, 1, 6, 2, 8];
-$sorted_array = sort_list($data);
 
 
-function sort_list($list)
-{
-    $sorted_data = [];
-    rsort($list);
-    foreach ($list as $item) {
-        $sorted_data[] += $item;
-    }
-    return $sorted_data;
-}
-
-function create_lists($group_count)
-{
-    $listArray = [];
-    for ($list = 0; $list < $group_count; $list++) {
-        array_push($listArray, array());
-    }
-    return $listArray;
-}
-function smallest_array($arrays)
-{
-    $lowestSum = array_sum($arrays[0]);
-    $lowestArrayKey = 0;
-    for ($i = 1; $i < count($arrays); $i++) {
-        $sum = array_sum($arrays[$i]);
-        if ($sum  < $lowestSum) {
-            $lowestSum = $sum;
-            $lowestArrayKey = $i;
-        }
-    }
-    return $lowestArrayKey;
-}
-
-function group_items($group_count, $sorted_list)
-{
-    $arrays = create_lists($group_count);
-    for ($i = 0; $i < count($sorted_list); $i++) {
-        array_push($arrays[smallest_array($arrays)], $sorted_list[$i]);
-    }
-    return $arrays;
-}
-print_r(group_items(3, $sorted_array));
+$obj = new ListClass($data);
+$obj->create_lists(3);
+$obj->group_items();
+print_r($obj);
